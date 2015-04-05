713bb2cde1ea26f405dfa9ceaaf03e71b947aa7b
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-04-06 03:56:31 +0530

    Separate out api repo

---
+++ README.md
@@ -0,0 +1,21 @@
+# Grouphone API
+
+API layer for Grouphone
+
+## Install
+
+- Give write permissions to the `storage` directory.
+- Copy `config.js.sample` to `config.js` in the same directory.
+- Change values in `config.js` if necessary.
+- `npm install`
+- `node server.js`
+
+## Run on different port
+
+The port to run the server can be overriden by passing the port number
+as third argument to `node server.js`. E.g. to run on port `80`, you
+would do this:
+
+```
+# node server.js 80
+```
---
+++ server.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+var express = require("express"),
+    bodyParser = require("body-parser"),
+    config = require("./config"),
+    db = require("./db");
+
+var app = express();
+
+// Override port from third commandline argument, if present
+if (process.argv && process.argv[2]) {
+    config.APP_PORT = parseInt(process.argv[2]);
+}
+
+// Set useful globals
+global.db = db,
+global.libs = require("./api/libs"),
+global.config = config,
+global.approot = __dirname + "/";
+
+// Configure application
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Register routes
+app.use("/", function (req, res) {
+  res.redirect("http://grouphone.me");
+});
+app.use("/api", require("./api"));
+
+// Start the server
+var server = app.listen(config.APP_PORT, config.APP_IP, function () {
+    console.log("Starting Grouphone API server...");
+    console.log("Listening on port %s:%d", config.APP_IP, config.APP_PORT);
+});
+
