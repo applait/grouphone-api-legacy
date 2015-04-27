e995ba80141b6cee7070c36c9a862f089e9dc2b0
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-04-27 21:17:47 +0530

    Remove levelDB dependency
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- db.js
+++ db.js
@@ -2,15 +2,10 @@
  * Adapter to different storage units
  */
 
-var levelup = require("levelup"),
-    config = require("./config"),
+var config = require("./config"),
     mongo = require("mongoskin").db(config.MONGO_URL);
 
 module.exports = {
-    invites: levelup("./storage/invites.db", {
-        keyEncoding: "json",
-        valueEncoding: "json"
-    }),
     activations: mongo.collection("activations"),
     accounts: mongo.collection("accounts"),
     sessions: mongo.collection("sessions"),
