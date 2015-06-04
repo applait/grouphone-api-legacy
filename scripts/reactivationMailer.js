83b6c94b366dafe7ed83d512bcb4c97f50e9be82
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-06-04 16:46:26 +0530

    Add end() to reactivationMailer.js
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- scripts/reactivationMailer.js
+++ scripts/reactivationMailer.js
@@ -10,10 +10,11 @@ var config = require("../config");
 global.config = config;
 global.db = require("../db");
 
+var count = 0,
+    length = 0;
+
 var mailerAPI = ["https://", config.APP_IP, ":", config.APP_PORT, "/mailer"].join(""),
     subject = "Activate your Grouphone account. We're waiting for you!";
-
-var libs = require("../api/libs");
 
 // Disable TLS authorization check so that we can use self-signed cert for the Grouphone API
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
@@ -26,6 +27,8 @@ db.activations.find().toArray(function (err, users) {
     process.exit();
   }
 
+  count = users.length;
+
   users.forEach(function (user) {
 
     request.post(
@@ -35,15 +38,26 @@ db.activations.find().toArray(function (err, users) {
       function (err, response, body) {
         if (!err && response.statusCode == 200) {
           body = JSON.parse(body);
+          length++;
           console.log("+ " + body.message);
         } else {
           body = JSON.parse(body);
           console.error(body.message);
+          length++;
           console.log("x Unable to send mailer to " + user.email);
         }
       });
 
   });
 
-
+  end();
 });
+
+var end = function () {
+  if (count === length) {
+    console.log("Done");
+    db.mongo.close();
+  } else {
+    setTimeout(function () { end(); }, 1000);
+  }
+};
